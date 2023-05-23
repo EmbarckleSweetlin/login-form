bea868da24d9e288e144b80b345280d4fc1542c8
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-23 21:06:16 +0530

    update files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app";
-// import "./index.css";
+import { App } from "./app.js";
+import "./app.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+  <img
+        id="logo"
+        src="https://econnect.embarckle.com/logo-text-light.63fac4b2.png"
+        alt="Loading"
+      />
     <App />
   </BrowserRouter>
 );
