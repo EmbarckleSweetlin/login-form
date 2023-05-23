69ca45f6124dbb90e7bcc0bdfbae51867fa938ed
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-23 21:02:22 +0530

    create the dashboard page

---
+++ src/dashboard-page/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './dashboard.css';
+
+export function Dashboard() {
+  return( 
+    <nav id="dashboard-nav">
+  <h1 id="dashboard-head">Dashboard</h1>
+  </nav>
+  )
+}
