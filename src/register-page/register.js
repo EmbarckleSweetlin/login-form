c0269fec4012dceea95e8390bf2b3d5431faa7a0
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-23 21:04:38 +0530

    register page with styles

--- src/register-page/register.js
+++ src/register-page/register.js
@@ -1,33 +1,96 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./register.css";
 
 export function SignupForm() {
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  const inputValues = (e) => {
     e.preventDefault();
+    if (password === confirmPassword) {
+      axios
+        .post("http://localhost:3001/signup", {
+          userName: userName,
+          email: email,
+          password: password,
+          //   confirmPassword: confirmPassword,
+        })
+        .then((res) => {
+          alert(res.data);
+          if (res.data === "you already have an account") {
+            alert("Please login");
+            navigate("/");
+          } else {
+            navigate("/verify", {
+              state: { email },
+            });
+          }
+        });
+    } else {
+      alert("password did not match");
+    }
   };
-  const handleSignup = () => {
-    // alert("Successfully Completed");
-    // return console.log("hello");
-    fetch("localhost:3000").then();
-  };
+
   return (
     <div id="signup-container">
       <h1>Signup Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label>User Name</label>
-        <input type="text" placeholder="user name"></input>
-        <label>Email</label>
-        <input type="email" placeholder="mail"></input>
-        <label>Password</label>
-        <input type="password" placeholder="password" autoComplete="on"></input>
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          placeholder="Confirm password"
-          autoComplete="on"
-        ></input>
-        <button onSubmit={handleSignup}>Signup</button>
-        <p>Already have an account?</p>
-        <a href="/">Login</a>
+      <form onSubmit={inputValues}>
+        <div className="signup-div">
+          <label>User Name</label>
+          <input
+            type="text"
+            placeholder="user name"
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="signup-div">
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="mail"
+            pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="signup-div">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="password"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+            autoComplete="on"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="signup-div">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Confirm password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+          ></input>
+        </div>
+
+        <input id="signup" type="submit" value="Signup" />
+        <div>
+          <p>Already have an account?</p>
+          <a href="/">Login</a>
+        </div>
       </form>
     </div>
   );
