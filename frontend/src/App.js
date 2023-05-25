27e4c254d7af99c7f10861999f96918c85527c50
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-25 13:32:46 +0530

    set the route path for each component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { LoginForm } from "./loginPage/login.js";
 import { SignupForm } from "./register-page/register.js";
-// import "./app.css";
+import { Dashboard } from "./dashboard-page/dashboard.js";
+import { NewPassword } from "./forgetPassword/forgetPassword.js";
+import { Otp } from "./verification-page/otp.js";
+import "./App.css";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" Component={LoginForm}></Route>
       <Route path="/signup" Component={SignupForm}></Route>
+      <Route path="/dashboard" Component={Dashboard}></Route>
+      <Route path="/forgetpassword" Component={NewPassword}></Route>
+      <Route path="/verify" Component={Otp}></Route>
     </Routes>
   );
 }
