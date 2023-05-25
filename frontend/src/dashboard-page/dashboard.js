8a9f999868db487ea5e3b5934a2b8f3d39fbce1b
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-25 17:50:27 +0530

    create logout signout buttons and style changes

--- frontend/src/dashboard-page/dashboard.js
+++ frontend/src/dashboard-page/dashboard.js
@@ -1,10 +1,44 @@
 import React from "react";
 import './dashboard.css';
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import axios from 'axios';
 
 export function Dashboard() {
-  return( 
-    <nav id="dashboard-nav">
+  const navigate = useNavigate();
+  const {state} = useLocation();
+  const handleClick = () => {
+  const confirm = window.confirm('Are you sure to logout');
+    if(confirm){
+      navigate('/');
+    }
+    else{
+      navigate('/dashboard')
+    }
+  }
+  const handlesignout = () => {
+   const confirm = window.confirm('Are you sure to signout');
+    if(confirm){
+      axios.post('http://localhost:3500/delete', {
+      email: state.email
+      })
+      .then(()=>{
+        navigate('/signup')
+      })
+      
+    } else{ 
+      navigate('/dashboard')
+    }
+  }
+  return( <div>
+    <div id="button-handle">
+    <button id="logoutButton" onClick={handleClick}>Logout</button>
+  <button id="signoutButton" onClick={handlesignout}>signout</button>
+  </div>
+    <div id="dashboard-nav">
   <h1 id="dashboard-head">Dashboard</h1>
-  </nav>
+  
+  </div>
+  </div>
   )
 }
