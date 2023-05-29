0a8e120ce83d0da50666cb7352eeedb7c9c49a65
juliet <sweetlinjuliet.h@embarckle.com>
2023-05-29 17:58:39 +0530

    added the eye icon in forgetpassword page

--- frontend/src/forgetPassword/forgetPassword.js
+++ frontend/src/forgetPassword/forgetPassword.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import {FaEyeSlash} from 'react-icons/fa';
 import './forgetPassword.css';
 
 
@@ -9,6 +11,8 @@ export function NewPassword() {
 const [email, setEmail] = useState();
 const [newpassword, setNewpassword] = useState();
 const [confirmpassword, setConfirmpassword] = useState();
+const [eyeon, setEyeon] = useState(false);
+  const [eyeon2, setEyeon2] = useState(false);
 const navigate = useNavigate();
 
 const handleClick = () =>{
@@ -47,12 +51,18 @@ axios
             <label class="forget-label" pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$">Email</label>
             <input id="forget-emailInput" className="forget-input" type="email" placeholder="enter valid mail address" onChange={(e)=>{setEmail(e.target.value)}} required></input>
             <label class="forget-label">New Password</label>
-            <input id="forget-passInput" className="forget-input" type="password" placeholder="create new password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            <input id="forget-passInput" className="forget-input" type={eyeon? "text" : "password"} placeholder="create new password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
             title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
             onChange={(e)=>{setNewpassword(e.target.value)}} required></input>
-            <label className="forget-label">Confirm Password</label>
-            <input id="forget-confirmInput" className="forget-input" type="password" placeholder="Enter password" onChange={(e)=>{setConfirmpassword(e.target.value)}} required></input>
-            <input id="forget-submit" type="submit"></input>
+           <div id="icons-forget" onClick={()=>{setEyeon(!eyeon)}}>
+            {eyeon? <FaEye color="black" /> :
+       <FaEyeSlash color="black"/>}</div>
+        <label className="forget-label">Confirm Password</label>
+            <input id="forget-confirmInput" className="forget-input" type={eyeon2? "text" : "password"} placeholder="Enter password" onChange={(e)=>{setConfirmpassword(e.target.value)}} required></input>
+            <div id="icons-forget2" onClick={()=>{setEyeon2(!eyeon2)}}>
+            {eyeon2? <FaEye color="black" /> :
+       <FaEyeSlash color="black"/>}</div>
+        <input id="forget-submit" type="submit"></input>
             <button id="cancel-button" onClick={handleClick}>Cancel</button>
         </form>
         </div>
